14b8d08622a51d2ce9da7010238a8a817598d998
Noah Berman <noah@noahberman.org>
2017-01-01 17:56:33 -0700

    db configured, add brew model

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 var express    = require('express');
 var app        = express(); 
 var bodyParser = require('body-parser');
+var mongoose   = require('mongoose'); 
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+mongoose.connect('mongodb://localhost/test')
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+});
+
 
 var port = process.env.PORT || 8080;
 
